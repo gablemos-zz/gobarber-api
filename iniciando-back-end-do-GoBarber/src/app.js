26757e188f5825ac9a42e24a1a14ccb0070b1fae
Gabriel Lemos <gabrielelias.lemos@gmail.com>
2020-02-05 07:50:13 -0200

    commit inicial

---
+++ iniciando-back-end-do-GoBarber/src/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import roustes from './routes'
+
+class App {
+  constructor() {
+    this.server = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares() {
+    this.server.use(express.json())
+  }
+
+  routes() {
+    this.server.use(roustes)
+  }
+}
+
+export default new App().server
